9aababb1e852fa7b6f9d394147afd14731e8f2c0
Alan Chen <alan.chen.au@gmail.com>
2021-05-05 21:01:35 +1000

    support oauth

--- src/main/java/com/telstra/phonenumbermanagement/security/SecurityConfig.java
+++ src/main/java/com/telstra/phonenumbermanagement/security/SecurityConfig.java
@@ -2,12 +2,17 @@ package com.telstra.phonenumbermanagement.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.telstra.phonenumbermanagement.filter.JwtRequestFilter;
 import com.telstra.phonenumbermanagement.service.impl.MyUserDetailsService;
 
 @EnableWebSecurity
@@ -16,9 +21,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private MyUserDetailsService myUserDetailsService;
 	
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter; 
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(myUserDetailsService); 
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+			.authorizeRequests().antMatchers("/authenticate").permitAll()
+			.anyRequest().authenticated()
+			.and().sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS); 
+		
+		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class); 
+	}
+	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean(); 
 	}
 	
 	@Bean
